33e43cebbf9e978e994bf5091e22bcac3fd4c300
JJadeYoon <yess9904@naver.com>
2024-01-14 13:10:57 +0900

    :sparkle: feature: 서비스 메서드 로직 작성

---
+++ src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
@@ -0,0 +1,38 @@
+package umc.spring.service.StoreService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.spring.domain.Review;
+import umc.spring.domain.Store;
+import umc.spring.repository.ReviewRepository;
+import umc.spring.repository.StoreRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class StoreQueryServiceImpl implements StoreQueryService {
+
+    private final StoreRepository storeRepository;
+    private final ReviewRepository reviewRepository;
+
+    @Override
+
+    public Optional<Store> findStore(Long id) {
+        return storeRepository.findById(id);
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long storeId, Integer page) {
+
+        Store store = storeRepository.findById(storeId).get();
+
+        Page<Review> StorePage = reviewRepository.findAllByStore(store, PageRequest.of(10, page));
+
+        return StorePage;
+    }
+}
